77e7965c2bc837f2c87cc80b52864393bd2be862
wurong wang <63132965@qq.com>
2019-01-10 11:29:00 +0800

    add a new app memorable places

---
+++ MemorablePlaces/MemorablePlaces/ViewController.swift
@@ -0,0 +1,117 @@
+//
+//  ViewController.swift
+//  MemorablePlaces
+//
+//  Created by 大兔子殿下 on 1/10/19.
+//  Copyright © 2019 大兔子殿下. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+
+    @IBOutlet var map: MKMapView!
+    var manager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // allow user to add new place by long pressing
+        let userLongPressGestureRecog = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.longPress(gestureRecognizer:)))
+        userLongPressGestureRecog.minimumPressDuration = 2
+        map.addGestureRecognizer(userLongPressGestureRecog)
+        
+        if activePlace == -1 {
+            
+            manager.delegate = self
+            manager.desiredAccuracy = kCLLocationAccuracyBest
+            manager.requestWhenInUseAuthorization()
+            manager.startUpdatingLocation()
+            
+        } else {
+            // get place details to display on map
+            if places.count > activePlace {
+                if let name = places[activePlace]["name"] {
+                    if let lat = places[activePlace]["lat"] {
+                        if let lon = places[activePlace]["lon"] {
+                            if let latitude = Double(lat) {
+                                if let longitude = Double(lon) {
+                                    let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+                                    let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+                                    let region = MKCoordinateRegion(center: coordinate, span: span)
+                                    self.map.setRegion(region, animated: true)
+                                    
+                                    // set the annotation
+                                    let annotation = MKPointAnnotation()
+                                    annotation.coordinate = coordinate
+                                    annotation.title = name
+                                    self.map.addAnnotation(annotation)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    @objc func longPress(gestureRecognizer: UIGestureRecognizer) {
+        if gestureRecognizer.state == UIGestureRecognizerState.began {
+            let touchPoint = gestureRecognizer.location(in: self.map)
+            let newCoordinate = self.map.convert(touchPoint, toCoordinateFrom: self.map)
+            let location = CLLocation(latitude: newCoordinate.latitude, longitude: newCoordinate.longitude)
+            var title = ""
+            
+            CLGeocoder().reverseGeocodeLocation(location, completionHandler: {(placemarks, error)
+                in
+                if error != nil {
+                    print(error)
+                } else {
+                    if let placemark = placemarks?[0] {
+                        if placemark.subThoroughfare != nil {
+                            title += placemark.subThoroughfare! + " "
+                        }
+                        if placemark.thoroughfare != nil {
+                            title += placemark.thoroughfare!
+                        }
+                    }
+                }
+                if title == "" {
+                    title = "Added \(NSDate())"
+                }
+                
+                // create the mark and add to the map
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = newCoordinate
+                annotation.title = title
+                self.map.addAnnotation(annotation)
+                
+                places.append(["name": title, "lat": String(newCoordinate.latitude), "lon": String(newCoordinate.longitude)])
+                // update the user dfaults as well
+                UserDefaults.standard.set(places, forKey: "places")
+                
+                print(places)
+                
+            })
+            
+        }
+    }
+    
+    @objc func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        self.map.setRegion(region, animated: true)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -16,3 +16,23 @@ A small game, allows two players and play again
 Just display the location and other related info. The most boring app.
 
 <img width="150" src="https://github.com/WangWurong/iOS-Basic-II/blob/master/Screenshots/location_aware.png" alt="Location Aware screenshot"/>
+
+### More interesting Map App: Memorable Places
+features:
+
+* Tab the name (table cell) to see the place on map
+* Press the "Add" button to add a new place, display the user's current location on the map
+* Long press on the map to add a new place
+* Get the name of the pressed location, if there is not a specific name, use the date
+* Delete the place by swiping the table cell
+* Permanently store the data
+
+<img width="150" src="https://github.com/WangWurong/iOS-Basic-II/blob/master/Screenshots/memorable_places_1.png" alt="Memorable Places screenshot"/>
+
+<img width="150" src="https://github.com/WangWurong/iOS-Basic-II/blob/master/Screenshots/memorable_places_2.png" alt="Memorable Places screenshot"/>
+
+<img width="150" src="https://github.com/WangWurong/iOS-Basic-II/blob/master/Screenshots/memorable_places_3.png" alt="Memorable Places screenshot"/>
+
+<img width="150" src="https://github.com/WangWurong/iOS-Basic-II/blob/master/Screenshots/memorable_places_4.png" alt="Memorable Places screenshot"/>
+
+<img width="150" src="https://github.com/WangWurong/iOS-Basic-II/blob/master/Screenshots/memorable_places_5.png" alt="Memorable Places screenshot"/>
